b7e33b7743e6c449f129aecbfaefcae0224df6af
奈鸭qwa <14312806+euiko@user.noreply.gitee.com>
2024-06-05 15:12:41 +0000

    艾梅莉埃v1
    Signed-off-by: 奈鸭qwa <14312806+euiko@user.noreply.gitee.com>

---
+++ meta-gs/character/艾梅莉埃v1/artis.js
@@ -0,0 +1,6 @@
+﻿export default function ({ attr, weapon, rule, def }) {
+  if (attr.mastery < 4000 ) {
+    return rule('艾梅莉埃-燃烧', { atk: 75, cpct: 100, cdmg: 100, mastery: 75, dmg: 100 , recharge: 35})
+  }
+  return def({ atk: 55, cpct: 100, cdmg: 100, mastery: 100, dmg: 100, recharge: 55 })
+}
