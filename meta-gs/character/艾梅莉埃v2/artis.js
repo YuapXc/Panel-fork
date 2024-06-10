73287b1068be7ebc54b234d37c6ece7a3e45397e
奈鸭qwa <14312806+euiko@user.noreply.gitee.com>
2024-06-10 18:01:47 +0000

    4.8v2
    Signed-off-by: 奈鸭qwa <14312806+euiko@user.noreply.gitee.com>

---
+++ meta-gs/character/艾梅莉埃v2/artis.js
@@ -0,0 +1,9 @@
+export default function ({ attr, weapon, rule, def }) {
+  if (attr.mastery > 99 ) {
+    return rule('艾梅莉埃-精通', { atk: 75, cpct: 100, cdmg: 100, mastery: 75, dmg: 100 , recharge: 35})
+  }
+  if (attr.mastery < 100 ) {
+    return rule('艾梅莉埃-常规', { atk: 85, cpct: 100, cdmg: 100, mastery: 0, dmg: 100 , recharge: 35})
+  }
+  return def({ atk: 55, cpct: 100, cdmg: 100, mastery: 100, dmg: 100, recharge: 55 })
+}
