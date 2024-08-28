98d6646d6797a3c1118c77d8f1a2f623892ac963
奈鸭qwa <14312806+euiko@user.noreply.gitee.com>
2024-08-28 16:28:23 +0000

    删除文件 meta-gs/character/卡齐娜

--- meta-gs/character/卡齐娜/artis.js
+++
@@ -1,6 +0,0 @@
-export default function ({ attr, rule, def ,weapon}) {
-  if (attr.mastery < 2190 ) {
-    return rule('卡齐娜-直伤', { hp: 0, atk: 0 ,def:100, cpct: 100, cdmg: 100, dmg: 100 , recharge: 30})
-  }
-  return def({ hp: 0, atk: 85, cpct: 100, cdmg: 100, mastery: 0, dmg: 100 , recharge: 0})
-}
