31f78c42ed2b21b1272e405fea1129046a5e0c49
奈鸭qwa <14312806+euiko@user.noreply.gitee.com>
2025-01-01 17:58:20 +0000

    删除文件 meta-gs/character

--- meta-gs/character/火主/artis.js
+++
@@ -1,9 +0,0 @@
-export default function ({ attr, rule, def ,weapon}) {
-  if (attr.mastery > 49 ) {
-    return rule('火主-蒸发', { atk: 75, cpct: 100, cdmg: 100, mastery: 75, dmg: 100 , recharge: 30})
-  }
-  if (attr.mastery < 50 ) {
-    return rule('火主-直伤', { atk: 85, cpct: 100, cdmg: 100, dmg: 100 , recharge: 30})
-  }
-  return def({ atk: 75, cpct: 100, cdmg: 100, mastery: 75, dmg: 100 })
-}
