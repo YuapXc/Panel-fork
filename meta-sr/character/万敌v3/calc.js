2c3fa3fb734d7b1d237fd7a91b9161952e7350e3
樱奈 <14312806+euiko@user.noreply.gitee.com>
2025-02-08 03:01:14 +0000

    万敌v5

---
+++ meta-sr/character/万敌v3/calc.js
@@ -0,0 +1,186 @@
+export const details = [{
+  title: '触发特效后生命值',
+  dmg: ({ attr, calc }) => {
+    return {
+      avg: Math.min(calc(attr.hp) * 1)
+    }
+  }
+}, {
+  title: '触发特效后攻击力',
+  dmg: ({ attr, calc }) => {
+    return {
+      avg: Math.min(calc(attr.atk) * 1)
+    }
+  }
+}, {
+  title: '触发特效后防御力',
+  dmg: ({ attr, calc }) => {
+    return {
+      avg: Math.min(calc(attr.def) * 1)
+    }
+  }
+}, {
+  title: '触发特效后速度',
+  dmg: ({ attr, calc }) => {
+    return {
+      avg: Math.min(calc(attr.speed) * 1)
+    }
+  }
+}, {
+  title: '触发特效后暴击率',
+  dmg: ({ attr, calc }) => {
+    return {
+      avg: Math.min(calc(attr.cpct) * 1)
+    }
+  }
+}, {
+  title: '触发特效后暴击伤害',
+  dmg: ({ attr, calc }) => {
+    return {
+      avg: Math.min(calc(attr.cdmg) * 1)
+    }
+  }
+}, {
+  title: '触发特效后击破特攻',
+  dmg: ({ attr, calc }) => {
+    return {
+      avg: Math.min(calc(attr.stance) * 1)
+    }
+  }
+}, {
+  title: '触发特效后效果命中',
+  dmg: ({ attr, calc }) => {
+    return {
+      avg: Math.min(calc(attr.effPct) * 1)
+    }
+  }
+}, {
+  title: '触发特效后效果抵抗',
+  dmg: ({ attr, calc }) => {
+    return {
+      avg: Math.min(calc(attr.effDef) * 1)
+    }
+  }
+}, {
+  title: '触发特效后充能效率',
+  dmg: ({ attr, calc }) => {
+    return {
+      avg: Math.min(calc(attr.recharge) * 1)
+    }
+  }
+}, {
+  title: '当前遗器套装',
+  dmg: ({ artis , attr, calc, talent }) => {
+    return {
+      avg: artis ,
+      type: 'text'
+    }
+  }
+},{
+  title: '[A]踏破征途的誓言伤害',
+  params: { XC: false , A: false},
+  dmg: ({ talent, attr }, { basic }) => basic(talent.a['技能伤害'] * attr.hp, 'a')
+},{
+  title: '[E]万死无悔主目标伤害',
+  params: { XC: false , A: false},
+  dmg: ({ talent, attr }, { basic }) => basic(talent.e['目标伤害'] * attr.hp, 'e')
+},{
+  title: '[E]万死无悔相邻目标伤害',
+  params: { XC: false , A: false},
+  dmg: ({ talent, attr }, { basic }) => basic(talent.e['相邻目标伤害'] * attr.hp, 'e')
+},{
+  title: '[Q]诛天焚骨的王座主目标伤害',
+  params: { XC: false , A: false},
+  dmg: ({ talent, attr }, { basic }) => basic(talent.q['目标伤害'] * attr.hp, 'q')
+},{
+  title: '[Q]诛天焚骨的王座相邻目标伤害',
+  params: { XC: false , A: false},
+  dmg: ({ talent, attr }, { basic }) => basic(talent.q['相邻目标伤害'] * attr.hp, 'q')
+},{
+  title: '[血仇]当前生命值上限',
+  params: { XC: true , A: false},
+  dmg: ({ attr, calc }) => {
+    return {
+      avg: Math.min(calc(attr.hp) * 1)
+    }
+  }
+},{
+  title: '[血仇]累计已损失生命值',
+  params: { XC: true , A: false},
+  dmg: ({ attr, calc }) => {
+    return {
+      avg: Math.min(calc(attr.hp) * 1.8)
+    }
+  }
+},{
+  title: '[E]弑王成王主目标伤害',
+  params: { XC: true , A: false},
+  dmg: ({ talent, attr }, { basic }) => basic(talent.e2['弑王成王目标伤害'] * attr.hp, 'e')
+},{
+  title: '[E]弑王成王相邻目标伤害',
+  params: { XC: true , A: false},
+  dmg: ({ talent, attr }, { basic }) => basic(talent.e2['弑王成王相邻目标伤害'] * attr.hp, 'e')
+},{
+  title: '[E]弑神登神主目标伤害',
+  dmgKey: 'AE',
+  check: ({ cons }) => cons < 1,
+  params: { XC: true , A: false},
+  dmg: ({ talent, attr }, { basic }) => basic(talent.e2['弑神登神目标伤害'] * attr.hp * 1.8, 'e')
+},{
+  title: '[E]弑神登神相邻目标伤害',
+  check: ({ cons }) => cons < 1,
+  params: { XC: true , A: false},
+  dmg: ({ talent, attr }, { basic }) => basic(talent.e2['弑神登神相邻目标伤害'] * attr.hp * 1.8, 'e')
+},{
+  title: '[E]弑神登神主目标伤害',
+  dmgKey: 'AE',
+  check: ({ cons }) => cons > 0,
+  params: { XC: true , A: false},
+  dmg: ({ talent, attr }, { basic }) => basic(talent.e2['弑神登神目标伤害'] * 1.3 * attr.hp * 1.8, 'e')
+},{
+  title: '[E]弑神登神其他全体目标伤害',
+  check: ({ cons }) => cons > 0,
+  params: { XC: true , A: false},
+  dmg: ({ talent, attr }, { basic }) => basic(talent.e2['弑神登神目标伤害'] * attr.hp * 1.8, 'e')
+},{
+  title: '[Q]诛天焚骨的王座主目标伤害',
+  params: { XC: true , A: false},
+  dmg: ({ talent, attr }, { basic }) => basic(talent.q['目标伤害'] * attr.hp, 'q')
+},{
+  title: '[Q]诛天焚骨的王座相邻目标伤害',
+  params: { XC: true , A: false},
+  dmg: ({ talent, attr }, { basic }) => basic(talent.q['相邻目标伤害'] * attr.hp, 'q')
+}]
+
+export const defDmgKey = 'AE'
+export const mainAttr = 'atk,cpct,cdmg'
+
+export const buffs = [{
+  check: ({ params,cons}) => params.XC === true,
+  title: '血仇：生命上限提高[hpPlus]点',
+  data: {
+    hpPlus: ({ talent, attr }) => attr.hp * 0.5
+ }
+},{
+  check: ({ calc, attr }) => calc(attr.hp) > 5000 ,
+  title: '血祥罩衫：暴击率提高[cpct]%',
+  data: {
+    cpct: ({ calc, attr }) => Math.min(114514, (calc(attr.hp) - 4000 ) / 100 * 1.2)
+ }
+}, {
+  check: ({ params }) => params.XC === true,
+  title: '血仇：无视敌方目标15%的防御力',
+  cons: 2,
+  data: {
+    ignore: 15
+ }
+}, {
+  check: ({ params }) => params.XC === true,
+  title: '血仇：暴击伤害提高30%',
+  cons: 4,
+  data: {
+    cdmg: 30
+ }
+}]
+
+export const createdBy = 'Ehya丶Calc'
