829c85b53ee10fa4e26870ec00ab531f07b7b80f
YuapXc <930319121@qq.com>
2025-03-21 17:29:17 +0800

    📝fix(meta-sr): 更新星铁版本至3.2v5

--- meta-sr/character/那刻夏/calc.js
+++ meta-sr/character/那刻夏/calc.js
@@ -1,32 +1,73 @@
 export const details = [{
   title: '[A]楚痛，酿造实识-伤害',
+  check: ({ cons }) => cons < 6,
   dmg: ({ talent }, dmg) => dmg(talent.a['技能伤害'], 'a')
 }, {
   title: '[E-单敌人]分形，驱逐虚知-单次伤害',
+  check: ({ cons }) => cons < 6,
   dmg: ({ talent }, dmg) => dmg(talent.e['目标伤害'], 'e')
 }, {
   title: '[E-单敌人]分形，驱逐虚知-全部伤害',
+  check: ({ cons }) => cons < 6,
   dmg: ({ talent }, dmg) => dmg(talent.e['目标伤害'] + talent.e['目标伤害'] * 4, 'e')
 }, {
   title: '[E-五敌人]分形，驱逐虚知-单次伤害',
+  check: ({ cons }) => cons < 6,
   params: { W: true},
   dmg: ({ talent }, dmg) => dmg(talent.e['目标伤害'], 'e')
 }, {
   title: '[E-五敌人]分形，驱逐虚知-全部伤害',
+  check: ({ cons }) => cons < 6,
   params: { W: true},
   dmgKey: 'AE',
   dmg: ({ talent }, dmg) => dmg(talent.e['目标伤害'] + talent.e['目标伤害'] * 4, 'e')
 }, {
   title: '[Q]生息破土，世界塑造-伤害',
+  check: ({ cons }) => cons < 6,
   dmg: ({ talent }, dmg) => dmg(talent.q['技能伤害'], 'q')
 }, {
   title: '[2命]生息破土，世界塑造-额外伤害',
-  check: ({ cons }) => cons > 1,
+  check: ({ cons }) => cons > 1 && cons < 6,
   dmg: ({ attr }, { basic }) => basic(attr.atk * 50 / 100, 'q')
 }, {
   title: '[2命]生息破土，世界塑造-额外总伤害',
-  check: ({ cons }) => cons > 1,
+  check: ({ cons }) => cons > 1 && cons < 6,
   dmg: ({ attr }, { basic }) => basic(attr.atk * 50 / 100 * 3, 'q')
+}, {
+  title: '[A]楚痛，酿造实识-伤害',
+  check: ({ cons }) => cons > 5,
+  dmg: ({ talent }, dmg) => dmg(talent.a['技能伤害'] * 1.3, 'a')
+}, {
+  title: '[E-单敌人]分形，驱逐虚知-单次伤害',
+  check: ({ cons }) => cons > 5,
+  dmg: ({ talent }, dmg) => dmg(talent.e['目标伤害'] * 1.3, 'e')
+}, {
+  title: '[E-单敌人]分形，驱逐虚知-全部伤害',
+  check: ({ cons }) => cons > 5,
+  dmg: ({ talent }, dmg) => dmg((talent.e['目标伤害'] + talent.e['目标伤害'] * 4) * 1.3, 'e')
+}, {
+  title: '[E-五敌人]分形，驱逐虚知-单次伤害',
+  check: ({ cons }) => cons > 5,
+  params: { W: true},
+  dmg: ({ talent }, dmg) => dmg(talent.e['目标伤害'] * 1.3, 'e')
+}, {
+  title: '[E-五敌人]分形，驱逐虚知-全部伤害',
+  check: ({ cons }) => cons > 5,
+  params: { W: true},
+  dmgKey: 'AE',
+  dmg: ({ talent }, dmg) => dmg((talent.e['目标伤害'] + talent.e['目标伤害'] * 4) * 1.3, 'e')
+}, {
+  title: '[Q]生息破土，世界塑造-伤害',
+  check: ({ cons }) => cons > 5,
+  dmg: ({ talent }, dmg) => dmg(talent.q['技能伤害'] * 1.3, 'q')
+}, {
+  title: '[2命]生息破土，世界塑造-额外伤害',
+  check: ({ cons }) => cons > 5,
+  dmg: ({ attr }, { basic }) => basic(attr.atk * 50 / 100 * 1.3, 'q')
+}, {
+  title: '[2命]生息破土，世界塑造-额外总伤害',
+  check: ({ cons }) => cons > 5,
+  dmg: ({ attr }, { basic }) => basic(attr.atk * 50 / 100 * 3 * 1.3, 'q')
 }]
 
 export const defDmgKey = 'AE'
@@ -72,10 +113,10 @@ export const buffs = [{
     atkPct: 60
   }
 }, {
-  title: '6命：造成的伤害提高30%',
+  title: '6命：造成的伤害为原伤害的130%，我方全体造成的伤害提高40%。',
   cons: 6,
   data: {
-    dmg: 30
+    dmg: 40
   }
 }]
 
