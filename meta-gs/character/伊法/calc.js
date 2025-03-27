227dcadf437436b1328412bc07b9fe0e54c20370
YuapXc <930319121@qq.com>
2025-03-27 13:30:25 +0800

    📝perf(meta-gs&meta-sr): 同步原库「GI5.6V1」

--- meta-gs/character/伊法/calc.js
+++ meta-gs/character/伊法/calc.js
@@ -1,45 +1,41 @@
 export const details = [{
-  title: '[A]普通攻击一段伤害',
-  dmg: ({ talent }, dmg) => dmg(talent.a['一段伤害'], 'a')
-}, {
-  title: '[A]普通攻击二段伤害',
-  dmg: ({ talent }, dmg) => dmg(talent.a['二段伤害'], 'a')
-}, {
-  title: '[A]普通攻击三段伤害',
-  dmg: ({ talent }, dmg) => dmg(talent.a['三段伤害'], 'a')
+  title: '[A]普通攻击伤害',
+  dmg: ({ talent, attr, cons }, dmg) => dmg(talent.a['一段伤害'] + talent.a['二段伤害'] + talent.a['三段伤害'], 'a')
 }, {
   title: '[A]重击伤害',
-  dmg: ({ talent }, dmg) => dmg(talent.a['重击伤害'], 'a')
+  dmg: ({ talent, attr, cons }, dmg) => dmg(talent.a['重击伤害'], 'a')
 }, {
   title: '[A]下坠期间伤害',
-  dmg: ({ talent }, dmg) => dmg(talent.a['下坠期间伤害'], 'a')
+  dmg: ({ talent, attr, cons }, dmg) => dmg(talent.a['下坠期间伤害'], 'a')
 }, {
   title: '[A]低空坠地冲击伤害',
-  dmg: ({ talent }, dmg) => dmg(talent.a['低空/高空坠地冲击伤害'][0], 'a')
+  dmg: ({ talent, attr, cons }, dmg) => dmg(talent.a['低空/高空坠地冲击伤害'][0], 'a')
 }, {
   title: '[A]高空坠地冲击伤害',
-  dmg: ({ talent }, dmg) => dmg(talent.a['低空/高空坠地冲击伤害'][1], 'a')
+  dmg: ({ talent, attr, cons }, dmg) => dmg(talent.a['低空/高空坠地冲击伤害'][1], 'a')
 }, {
   title: '[E]秘药弹伤害',
-  dmg: ({ talent }, dmg) => dmg(talent.e['秘药弹伤害'], 'e')
+  dmg: ({ talent, attr, cons }, dmg) => dmg(talent.e['秘药弹伤害'], 'e')
 }, {
-  title: '[E]秘药弹命中治疗量',
-  dmg: ({ attr, talent }, { heal }) => heal(attr.mastery * talent.e['秘药弹命中治疗量'][0] + talent.e['秘药弹命中治疗量'][1])
+  title: `[E]秘药弹命中治疗量`,
+  dmgKey: 'AE',
+  dmg: ({ talent, attr, calc }, { heal }) =>
+    heal(talent.e['秘药弹命中治疗量2'][0] * calc(attr.mastery) / 100 + talent.e['秘药弹命中治疗量2'][1])
 }, {
   title: '[Q]复合镇静域伤害',
-  dmg: ({ talent }, dmg) => dmg(talent.q['技能伤害'], 'q')
+  dmg: ({ talent, attr, cons }, dmg) => dmg(talent.q['技能伤害'], 'q')
 }, {
   title: '[Q]镇静标记伤害',
-  dmg: ({ talent }, dmg) => dmg(talent.q['镇静标记伤害'], 'q')
-}];
+  dmg: ({ talent, attr, cons }, dmg) => dmg(talent.q['镇静标记伤害'], 'q')
+}]
 
-export const defDmgKey = 'Q';
-export const mainAttr = 'atk,mastery,cpct,cdmg';
+export const defDmgKey = 'AE'
+export const mainAttr = 'atk,hp,cpct,cdmg,heal'
 
 export const buffs = [{
-  title: '被动天赋：元素精通提升100点',
+  title: '互助救援协议：元素精通提升80点',
   data: {
-    mastery: 100
+    mastery: 80
   }
 }, {
   title: '4命：元素精通提升100点',
@@ -58,6 +54,6 @@ export const buffs = [{
   data: {
     swirlDmg: 20
   }
-}];
+}]
 
-export const createdBy = 'YuapXc';
+export const createdBy = 'YuapXc'
--- meta-gs/character/爱可菲/calc.js
+++ meta-gs/character/爱可菲/calc.js
@@ -1,51 +1,46 @@
 export const details = [{
-  title: '[A1]后厨手艺一段伤害',
-  params: {team: false},
-  dmg: ({ talent }, dmg) => dmg(talent.a['一段伤害'], 'a')
-}, {
-  title: '[A2]后厨手艺二段伤害',
-  params: {team: false},
-  dmg: ({ talent }, dmg) => dmg(talent.a['二段伤害'], 'a')
-}, {
-  title: '[A3]后厨手艺三段伤害',
-  params: {team: false},
-  dmg: ({ talent }, dmg) => dmg(talent.a['三段伤害'], 'a')
+  title: '[A]后厨手艺普通攻击伤害',
+  dmg: ({ talent, attr, cons }, dmg) => dmg(talent.a['一段伤害'] + talent.a['二段伤害'] + talent.a['三段伤害'][0] + talent.a['三段伤害'][1], 'a')
 }, {
   title: '[A+]后厨手艺重击伤害',
-  params: {team: false},
-  dmg: ({ talent }, dmg) => dmg(talent.a['重击伤害'], 'a2')
+  dmg: ({ talent, attr, cons }, dmg) => dmg(talent.a['重击伤害'], 'a2')
 }, {
-  title: '[A-]后厨手艺下坠期间伤害',
-  params: {team: false},
-  dmg: ({ talent }, dmg) => dmg(talent.a['下坠期间伤害'], 'a2')
-}, {
-  title: '[A-]后厨手艺低空下落伤害',
-  params: {team: false},
-  dmg: ({ talent }, dmg) => dmg(talent.a['低空/高空坠地冲击伤害'][0], 'a3')
-}, {
-  title: '[A-]后厨手艺高空下落伤害',
-  params: {team: false},
-  dmg: ({ talent }, dmg) => dmg(talent.a['低空/高空坠地冲击伤害'][1], 'a3')
+  title: '[A-]后厨手艺下坠伤害',
+  dmg: ({ talent, attr, cons }, dmg) => dmg(talent.a['下坠期间伤害'] + talent.a['低空/高空坠地冲击伤害'][0] + talent.a['低空/高空坠地冲击伤害'][1], 'a2')
 }, {
   title: '[E]低温烹饪技能伤害',
-  params: {team: false},
-  dmg: ({ talent }, dmg) => dmg(talent.e['技能伤害'], 'e')
+  dmg: ({ talent, attr, cons }, dmg) => dmg(talent.e['技能伤害'], 'e')
+}, {
+  title: '[E]低温烹饪融化伤害',
+  dmg: ({ talent, attr, cons }, dmg) => dmg(talent.e['技能伤害'], 'e', 'melt')
 }, {
   title: '[E]冻霜芭菲伤害',
-  params: {team: false},
-  dmg: ({ talent }, dmg) => dmg(talent.e['冻霜芭菲伤害'], 'e')
+  dmg: ({ talent, attr, cons }, dmg) => dmg(talent.e['冻霜芭菲伤害'], 'e')
+}, {
+  title: '[E]冻霜芭菲融化伤害',
+  dmg: ({ talent, attr, cons }, dmg) => dmg(talent.e['冻霜芭菲伤害'], 'e', 'melt')
+}, {
+  title: '[Q]冻霜芭菲伤害',
+  dmg: ({ talent, attr, cons }, dmg) => dmg(talent.q['技能伤害'], 'q')
+}, {
+  title: '[Q]冻霜芭菲融化伤害',
+  dmg: ({ talent, attr, cons }, dmg) => dmg(talent.q['技能伤害'], 'q', 'melt')
 }, {
   title: '[Q]花刀技法技能伤害',
-  params: {team: false},
-  dmg: ({ talent }, dmg) => dmg(talent.q['技能伤害'], 'q')
+  dmg: ({ talent, attr, cons }, dmg) => dmg(talent.q['技能伤害'], 'q')
 }, {
-  title: '[Q]花刀技法治疗量',
-  params: {team: false},
-  heal: ({ talent, attr }, heal) => heal(talent.q['治疗量'], attr.atk)
-}];
+  title: `[Q]花刀技法治疗量`,
+  dmgKey: 'AE',
+  dmg: ({ talent, attr, calc }, { heal }) =>
+    heal(talent.q['治疗量2'][0] * calc(attr.atk) / 100 + talent.q['治疗量2'][1])
+}, {
+  title: `[T]康复食疗治疗量`,
+  dmg: ({ talent, attr, calc }, { heal }) =>
+    heal(calc(attr.atk) * 1.3824)
+}]
 
-export const defDmgKey = 'Q';
-export const mainAttr = 'atk,cpct,cdmg,heal';
+export const defDmgKey = 'AE'
+export const mainAttr = 'atk,hp,cpct,cdmg,heal'
 
 export const buffs = [{
   title: '爱可菲被动：花刀技法治疗量提升[healPct]%',
@@ -53,11 +48,22 @@ export const buffs = [{
     healPct: 20
   }
 }, {
+  title: '灵感浸入调味：冰元素抗性降低5%',
+  data: {
+    kx: 5
+  }
+}, {
   check: ({ params }) => params.team === true,
   title: '队伍增益：提升全队冰元素伤害[iceDmg]%',
   data: {
     iceDmg: 15
   }
-}];
+}, {
+  title: '1命：造成冰元素伤害时的暴击伤害提升60%。',
+  cons: 1,
+  data: {
+    cdmg: 60
+  }
+}, 'melt']
 
-export const createdBy = 'YuapXc';
+export const createdBy = 'YuapXc'
