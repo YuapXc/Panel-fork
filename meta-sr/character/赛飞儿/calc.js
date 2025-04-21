540164228680661cd6647854c8259552f99bdcf0
YuapXc <930319121@qq.com>
2025-04-21 22:53:40 +0800

    📝perf(meta-sr): 同步原库「HSR3.3V3」

--- meta-sr/character/赛飞儿/calc.js
+++ meta-sr/character/赛飞儿/calc.js
@@ -31,57 +31,51 @@ export const details = [
 
 export const defDmgIdx = 3
 export const defParams = { oldCustomer: true }
-export const mainAttr = "atk,cpct,cdmg"
+export const mainAttr = 'atk,cpct,cdmg,speed'
 
-export const buffs = [
-  {
-    title: '天赋：追加攻击触发，造成等同于赛飞儿攻击力[tDmg]%的量子属性伤害',
-    tree: 1,
-    data: {
-      tDmg: ({ talent }) => talent.t["技能伤害"] * 100
-    }
-  },
-  {
-    title: '战技：攻击力提高[_atkBuff]%',
-    tree: 1,
-    data: {
-      _atkBuff: ({ talent }) => talent.e["攻击力提高"] * 100
-    }
-  },
-  {
-    check: ({ cons, params, attr }) =>  attr.speed >= 140,
+export const buffs = [{
+  title: '嘿，空手套白银：攻击力提高[atkPct]%',
+  data: {
+    atkPct: 30
+  }
+}, {
+  title: '偷天换日：天赋的追加攻击造成的暴击伤害提高100%。赛飞儿在场时，敌方全体目标受到的伤害提高40%。',
+  data: {
+    tdmg: 100,
+    enemydmg: 40
+  }
+}, {
+  check: ({ cons, params, attr }) =>  attr.speed >= 140,
     title: '神行宝鞋：暴击率提高[cpct]%',
-    data: {
-      cpct: 25
-    }
+  data: {
+    cpct: 25
+  }
   },
   {
-    check: ({ cons, params, attr }) =>  attr.speed >= 170,
+  check: ({ cons, params, attr }) =>  attr.speed >= 170,
     title: '神行宝鞋：暴击率再提高[cpct]%',
-    data: {
-      cpct: 25
-    }
-  },
-  {
-    title: '赛飞儿1命：目标受到的伤害提高[enemydmg]%',
-    cons: 1,
-    data: {
-      enemydmg: 25
-    }
-  },
-  {
-    title: '赛飞儿2命：暴击伤害提高80%',
-    cons: 2,
-    data: {
-      cdmg: 80
-    }
+  data: {
+    cpct: 25
+  }
+}, {
+  title: '1命：攻击力提高80%',
+  cons: 1,
+  data: {
+    atkPct: 80
+  }
+}, {
+  title: '2命：受到的伤害提高30%',
+  cons: 2,
+  data: {
+    enemydmg: 30
+  }
   },
   {
     title: '赛飞儿6命：天赋追加攻击伤害提高[tdmg]%',
-    cons: 6,
-    data: {
-      tdmg: 350
-    }
+  cons: 6,
+  data: {
+    tdmg: 350
+  }
   }
 ]
 
