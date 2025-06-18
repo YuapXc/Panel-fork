71ed5e6428c8cf7f2d55d944995e421495edfa17
YuapXc <930319121@qq.com>
2025-06-19 01:28:36 +0800

    📝res(meta-gs): 更新伊涅芙伤害计算&评分 新反应机制「月感电」目前并没有写计算，暂时按照「夜魂」计算🤓

---
+++ meta-gs/character/伊涅芙/calc.js
@@ -0,0 +1,58 @@
+export const details = [
+  {
+    title: "[A]除尘旋刃·普攻伤害",
+    dmg: ({ talent }, dmg) => dmg(talent.a["一段伤害"] + talent.a["二段伤害"] + talent.a["三段伤害"] + talent.a["四段伤害"], "a")
+  },
+  {
+    title: "[E]涤净模式·稳态载频伤害",
+    dmg: ({ talent }, dmg) => dmg(talent.e["技能伤害"], "e")
+  },
+  {
+    title: "[E]涤净模式·稳态载频护盾量",
+    dmg: ({ attr, calc, talent }, { shield }) => shield(talent.e['护盾吸收量2'][1] + calc(attr.atk) * talent.e['护盾吸收量2'][0] / 100)
+  },
+  {
+    title: "[E+]薇尔琪塔放电伤害",
+    dmg: ({ talent }, dmg) => dmg(talent.e["薇尔琪塔放电伤害"], "e2")
+  },
+  {
+    title: "[Q]至高律令·全域扫灭伤害",
+    dmg: ({ talent }, dmg) => dmg(talent.q["技能伤害"], "q")
+  },
+  {
+    title: "天赋「频率超限回路」伤害",
+    dmg: ({ }, dmg) => dmg(80, "nightsoul")
+  },
+  {
+    title: "二命「辅助清理模块」伤害",
+    check: ({ cons }) => cons > 1,
+    dmg: ({ }, dmg) => dmg(400, "nightsoul")
+  },
+  {
+    title: "六命「献予你的明晨」伤害",
+    check: ({ cons }) => cons === 6,
+    dmg: ({ }, dmg) => dmg(200, "nightsoul")
+  }
+];
+
+export const defDmgIdx = 4;
+export const mainAttr = "atk,cpct,cdmg,dmg";
+
+
+export const buffs = [
+  {
+    title: "[天赋]月兆祝赐·象拟中继：每100点攻击力都将提升0.7%基础伤害，至多通过这种方式提升14%伤害",
+    data: {
+      dmg: ({ attr, calc }) => Math.min(calc(attr.atk) / 100 * 0.7, 14)
+    }
+  },
+  {
+    title: "1命「载流复合」效果：使月感电反应造成的伤害提升，每100点攻击力都将提升2.5%伤害，至多通过这种方式提升50%伤害",
+    cons: 1,
+    data: {
+      dmg: ({ attr, calc }) => Math.min(calc(attr.atk) / 100 * 2.5, 50)
+    }
+  }
+];
+
+export const createdBy = "YuapXc";
